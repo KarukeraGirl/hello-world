68a79286308e8481703c81e667057ed1b445c32f
Leslie STRAZEL <lstrazel@gmail.com>
2019-03-06 16:08:00 +0100

    Update checkCashRegister.js

--- checkCashRegister.js
+++ checkCashRegister.js
@@ -1,7 +1,19 @@
-function checkCashRegister(price, cash) {
+function checkCashRegister(price, cash, cid) {
   var change;
   // Here is your change, ma'am.
-  change=cash-price;
+ var totalCid=cid.reduce((a,b)=>a+b);
   return change;
 }
 
+// Example cash-in-drawer array:
+// [["PENNY", 1.01],
+// ["NICKEL", 2.05],
+// ["DIME", 3.1],
+// ["QUARTER", 4.25],
+// ["ONE", 90],
+// ["FIVE", 55],
+// ["TEN", 20],
+// ["TWENTY", 60],
+// ["ONE HUNDRED", 100]]
+
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
